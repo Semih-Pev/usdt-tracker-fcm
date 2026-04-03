af25c1a6d9951372ef7193904b97cb8534921f66
Semih-Pev <semihgulden2@gmail.com>
2026-04-04 00:42:23 +0300

    chore: organize repository into backend and frontend directories

--- backend/dist/firebase/firebase.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnApplicationBootstrap } from '@nestjs/common';
-export declare class FirebaseService implements OnApplicationBootstrap {
-    private readonly logger;
-    onApplicationBootstrap(): void;
-    sendMulticast(tokens: string[], data: any, title: string, body: string): Promise<import("node_modules/firebase-admin/lib/messaging/messaging-api").BatchResponse | undefined>;
-}
--- backend/dist/token/token.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class TokenService {
-    private readonly logger;
-    private tokens;
-    registerToken(token: string): void;
-    getAllTokens(): string[];
-}
